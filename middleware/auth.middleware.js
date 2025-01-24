5b6602ba528192b190dd197457c0591823964665
NifilKhan <nifilkhan.stackup@gmail.com>
2025-01-24 12:43:05 +0530

    added some checks for user,otp and cookie if their is error it will return errors

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -1,20 +1,39 @@
 import jwt from "jsonwebtoken";
+import {  verifyToken } from "../services/user.service.js";
 
-export const authenticatedUser = (req, res, next) => {
+export const authenticatedUser = async(req, res, next) => {
   try {
     console.log('token in middleware',req.cookies)
     const token = req.cookies.Authorization;
     console.log('token from middleware',token)
 
-    if(token){
-      const decode = jwt.verify(token,process.env.JWT_SECRET);
-  
-      console.log('user decoded value',decode)
+    if (!token) {
+      return res.status(401).json({ message: "Authentication token missing or invalid" });
+    }
+
+      const decode = verifyToken(token);
+      console.log("Decoded token:", decode);
+
+      if (!decode?.userId) {
+        return res.status(401).json({ message: "Invalid token payload" });
+      }
+
+      // console.log('user decoded value',decode)
+
+      // const user = await checkUserExists(decode.userId);
+
+      // console.log('user from decoded value in midd',user)
+      // if (!user) {
+      //   return res.status(401).json({ message: "User not found" });
+      // }
   
       req.userId = decode?.userId;
       req.role = decode?.role;
+
+      console.log('user id:',req.userId)
+      
   
-    }
+
     next();
   } catch (error) {
     if (error.name === "TokenExpiredError") {
